21466ba16de7200e11de89203cecfb1324ac1684
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-18 17:48:18 +0300

    Done task

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
 fun main() {
-    println("Hello World!")
+    // part 1
+var age:Int = 28
+    // age = null : won't work
+
+// part 2
+    var city:String? = "Glasgow"
+    println(city?.length)
+    city = null // works like magic
+
+    // part 3
+    println(city?.length)
+
+    // part 4
+    var defaultCity = city?:"Kuwait"
+    println(defaultCity)
+
+    println(getScore(null))
+    println(getScore(30.0))
+
+
+
+}
+
+
+// 5
+fun getScore(score: Double?):Double?{
+    var result = score?:10.0
+    return result
 }
